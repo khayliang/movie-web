dfd20d1e2f8e32127f220d8cdccbd8269f8f32a5
khayliang <khayliang@gmail.com>
2020-12-17 21:18:13 +0800

    fix typo in api route

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import MovieData from '../types/movieDataInterface';
 
 export const getMovies = async (): Promise<MovieData[]> => {
   const response = await request(
-    'https://dhvye6e1ul.execute-api.us-east-1.amazonaws.com/dev/get_speech',
+    'https://dhvye6e1ul.execute-api.us-east-1.amazonaws.com/dev/get_movies',
     {
       method: 'get',
     },
